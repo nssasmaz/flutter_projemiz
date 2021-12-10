25cbd3d7c956d9a52e9167346d64f72dcf4176e2
Nursin Şaşmaz <nssasmaz@gmail.com>
2021-12-10 08:21:56 +0300

    Update oturum_ac.dart

--- lib/ekranlar/oturum_ac.dart
+++ lib/ekranlar/oturum_ac.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_projemiz/sistem/nKullanici.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+import 'dart:io';
 
 class OturumAc2 extends StatefulWidget {
   @override
@@ -9,11 +12,85 @@ class OturumAc2 extends StatefulWidget {
 class _OturumAcState extends State<OturumAc2> {
   @override
   Widget build(BuildContext context) {
-    final kullaniciAdi = new TextEditingController();
-    final sifre = new TextEditingController();
+    bool _oturum_durumu = false;
+    Kullanici _Kullanici;
+    var sonuc = null;
 
-    bool hata, islem_devam, basarili;
-    String sonuc_mesaj;
+    final formKullaniciAdi = TextEditingController();
+    final formSifre = TextEditingController();
+
+    void _oturumAc(String _kkod, String _sifre) async {
+      if (_kkod == '' || _sifre == '') {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Text("Kullanıcı adı veya şifre boş olamaz!"),
+            );
+          },
+        );
+      } else {
+        // GET METODU
+        var request = http.Request(
+            'GET',
+            Uri.parse(
+                'http://profesor.elazig.yerel/ajax.php?islem=oturumAc&kullanici_kodu=' +
+                    _kkod +
+                    '&sifre=' +
+                    _sifre));
+
+        /*
+    // POST METODU
+    var request = http.MultipartRequest(
+        'POST', Uri.parse('http://profesor.elazig.yerel/ajax.php'));
+    request.fields.addAll({'kullanici_adi': 'nssasmaz', 'sifre': 'tmf7qkckct'});
+    */
+
+        http.StreamedResponse response = await request.send();
+        if (response.statusCode == 200) {
+          var sonucJSON = await response.stream.bytesToString();
+          var sonuc = convert.jsonDecode(sonucJSON) as Map<String, dynamic>;
+          print(sonuc);
+          if (sonuc['durum'] == "tamam") {
+            setState(() {
+              _Kullanici = Kullanici.fromJson(sonuc['veri']);
+              _oturum_durumu = true;
+            });
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  // Retrieve the text the that user has entered by using the
+                  // TextEditingController.
+                  content: Text(sonuc['veri']['isim']),
+                );
+              },
+            );
+          } else {
+            print(sonuc['hata']);
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text(sonuc['hata'][0]['baslik']),
+                  // Retrieve the text the that user has entered by using the
+                  // TextEditingController.
+                  content: Text(sonuc['hata'][0]['aciklama']),
+                  actions: <Widget>[
+                    TextButton(
+                      child: const Text('Tamam'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          }
+        }
+      }
+    }
 
     Size boyut = MediaQuery.of(context).size;
     return Scaffold(
@@ -53,7 +130,7 @@ class _OturumAcState extends State<OturumAc2> {
                             labelStyle:
                                 TextStyle(fontSize: 16.0, color: Colors.white),
                             labelText: 'Kullanıcı Adı'),
-                        controller: kullaniciAdi,
+                        controller: formKullaniciAdi,
                       ),
                       SizedBox(height: 20),
                       TextField(
@@ -71,7 +148,7 @@ class _OturumAcState extends State<OturumAc2> {
                             labelStyle:
                                 TextStyle(fontSize: 16.0, color: Colors.white),
                             labelText: 'Şifre'),
-                        controller: sifre,
+                        controller: formSifre,
                       ),
                       SizedBox(height: 30),
 
@@ -109,51 +186,8 @@ class _OturumAcState extends State<OturumAc2> {
                                 style: TextStyle(color: Colors.white),
                               ),
                               onPressed: () async {
-                                var request = http.MultipartRequest(
-                                    'POST',
-                                    Uri.parse(
-                                        'http://mor.podkobi.com/webservis_panel/oturum.php?i=oturum-ac'));
-                                request.fields.addAll({
-                                  'kullanici_adi': kullaniciAdi.text,
-                                  'sifre': sifre.text
-                                });
-/*
-                                var headers = {
-                                  'Cookie':
-                                  'PHPSESSID=cvn198iugevio3ocek99gk98u6'
-                                };
-                                request.headers.addAll(headers);
-
-
- */
-
-                                http.StreamedResponse response =
-                                    await request.send();
-
-                                if (response.statusCode == 200) {
-                                  sonuc_mesaj =
-                                      await response.stream.bytesToString();
-                                } else {
-                                  sonuc_mesaj = response.reasonPhrase;
-                                }
-
-                                showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      title: Text("Oturum Açma İşlemi"),
-                                      content: Text("Sonuç : ${sonuc_mesaj}"),
-                                      actions: [
-                                        TextButton(
-                                          child: Text("KAPAT"),
-                                          onPressed: () {
-                                            Navigator.pop(context, true);
-                                          },
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
+                                _oturumAc(
+                                    formKullaniciAdi.text, formSifre.text);
                               },
                               style: ElevatedButton.styleFrom(
                                   primary: Colors.pink,
